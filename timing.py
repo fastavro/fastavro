e9bfdb176bb293e6056b505b4577840e786e5112
Miki Tebeka <miki.tebeka@gmail.com>
2011-11-08 20:01:58 -0800

    Skip javro

--- timing.py
+++ timing.py
@@ -4,32 +4,34 @@ import sys
 from os import environ
 from time import time
 
-sys.path.append('{HOME}/work/bdw-etl'.format(**environ))
-from bushido.common.avro import java_iter_avro
+def main():
+    filename = sys.argv[1]
 
-filename = sys.argv[1]
+    sys.path.append('{HOME}/work/bdw-etl'.format(**environ))
+    try:
+        from bushido.common.avro import java_iter_avro
+        start = time()
+        for i, record in enumerate(java_iter_avro(filename), 1):
+            pass
+        t = time() - start
+        print('javro: {0} [{1} records]'.format(t, i))
+    except ImportError:
+        print('Skipping javro')
 
+    from fastavro import iter_avro
+    start = time()
+    for i, record in enumerate(iter_avro(open(filename, 'rb')), 1):
+        pass
+    t = time() - start
+    print('fastavro: {0} [{1} records]'.format(t, i))
 
-start = time()
-for i, record in enumerate(java_iter_avro(filename), 1):
-    pass
-t = time() - start
-print('javro: {0} [{1} records]'.format(t, i))
+    import avro.io, avro.datafile
+    fo = open(filename, 'rb')
+    df = avro.datafile.DataFileReader(fo, avro.io.DatumReader())
+    start = time()
+    for i, record in enumerate(df, 1):
+        pass
 
-from fastavro import iter_avro
-start = time()
-for i, record in enumerate(iter_avro(open(filename, 'rb')), 1):
-    pass
-t = time() - start
-print('fastavro: {0} [{1} records]'.format(t, i))
-
-import avro.io, avro.datafile
-fo = open(filename, 'rb')
-df = avro.datafile.DataFileReader(fo, avro.io.DatumReader())
-start = time()
-for i, record in enumerate(df, 1):
-    pass
-
-t = time() - start
-print('avro: {0} [{1} records]'.format(t, i))
+    t = time() - start
+    print('avro: {0} [{1} records]'.format(t, i))
 
