d65dd38e347c4804df9ef5a3021b5313540d080b
Miki Tebeka <miki.tebeka@gmail.com>
2011-12-02 09:49:36 -0800

    Select interpreter

--- test.sh
+++ test.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+set -e
+
+python=${PYTHON:python}
+
+echo "Using $python interpreter"
+echo
+
 for avro in avro-files/*.avro;
 do
     echo $avro
-    ./fastavro.py -q "$avro"
+    $python ./fastavro.py -q "$avro"
 done
