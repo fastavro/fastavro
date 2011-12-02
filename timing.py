a1b7e684be72e8758d33c910aca64333b75b8813
Miki Tebeka <miki.tebeka@gmail.com>
2011-12-02 08:55:56 -0800

    Make avro optional

--- timing.py
+++ timing.py
@@ -4,14 +4,25 @@ import sys
 from os import environ
 from time import time
 
-def main():
-    filename = sys.argv[1]
+def main(argv=None):
+    import sys
+    from argparse import ArgumentParser
+
+    argv = argv or sys.argv
+
+    parser = ArgumentParser(description='Run timing info')
+    parser.add_argument('avro_file', help='Avro file for iterate')
+    parser.add_argument('--pyavro', default=False, action='store_true',
+                        help='run the avro python benchmark as well')
+
+    args = parser.parse_args(argv[1:])
+
 
     sys.path.append('{HOME}/work/bdw-etl'.format(**environ))
     try:
         from bushido.common.avro import java_iter_avro
         start = time()
-        for i, record in enumerate(java_iter_avro(filename), 1):
+        for i, record in enumerate(java_iter_avro(args.avro_file), 1):
             pass
         t = time() - start
         print('javro: {0} [{1} records]'.format(t, i))
@@ -20,20 +31,21 @@ def main():
 
     from fastavro import iter_avro
     start = time()
-    for i, record in enumerate(iter_avro(open(filename, 'rb')), 1):
+    for i, record in enumerate(iter_avro(open(args.avro_file, 'rb')), 1):
         pass
     t = time() - start
     print('fastavro: {0} [{1} records]'.format(t, i))
 
-#     import avro.io, avro.datafile
-#     fo = open(filename, 'rb')
-#     df = avro.datafile.DataFileReader(fo, avro.io.DatumReader())
-#     start = time()
-#     for i, record in enumerate(df, 1):
-#         pass
-# 
-#     t = time() - start
-#     print('avro: {0} [{1} records]'.format(t, i))
+    if args.pyavro:
+        import avro.io, avro.datafile
+        fo = open(filename, 'rb')
+        df = avro.datafile.DataFileReader(fo, avro.io.DatumReader())
+        start = time()
+        for i, record in enumerate(df, 1):
+            pass
+
+        t = time() - start
+        print('avro: {0} [{1} records]'.format(t, i))
 
 if __name__ == '__main__':
     main()
