e843fe91090cbb9e3750acae8c561a5995a7e252
Miki Tebeka <miki.tebeka@gmail.com>
2011-12-01 20:14:42 -0800

    Readme

--- timing.py
+++ timing.py
@@ -15,8 +15,8 @@ def main():
             pass
         t = time() - start
         print('javro: {0} [{1} records]'.format(t, i))
-    except ImportError:
-        print('Skipping javro')
+    except ImportError, e:
+        print('Skipping javro ({0})'.format(e))
 
     from fastavro import iter_avro
     start = time()
@@ -25,15 +25,15 @@ def main():
     t = time() - start
     print('fastavro: {0} [{1} records]'.format(t, i))
 
-    import avro.io, avro.datafile
-    fo = open(filename, 'rb')
-    df = avro.datafile.DataFileReader(fo, avro.io.DatumReader())
-    start = time()
-    for i, record in enumerate(df, 1):
-        pass
-
-    t = time() - start
-    print('avro: {0} [{1} records]'.format(t, i))
+#     import avro.io, avro.datafile
+#     fo = open(filename, 'rb')
+#     df = avro.datafile.DataFileReader(fo, avro.io.DatumReader())
+#     start = time()
+#     for i, record in enumerate(df, 1):
+#         pass
+# 
+#     t = time() - start
+#     print('avro: {0} [{1} records]'.format(t, i))
 
 if __name__ == '__main__':
     main()
