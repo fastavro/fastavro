736d1a559b561fc35e33632fb2a79faae95b2385
Miki Tebeka <miki.tebeka@gmail.com>
2012-01-11 17:11:09 +0200

    Use same source and conditional install
    --HG--
    branch : cython

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
+from shutil import copy
 
 try:
-    from distutils.extension import Extension
     from Cython.Distutils import build_ext
-    fastavro = Extension('fastavro.cfastavro', ['fastavro/cfastavro.pyx'])
+    from distutils.extension import Extension
+
+    pyx = 'fastavro/cfastavro.pyx'
+    copy('fastavro/pyfastavro.py', pyx)
+
+    fastavro = Extension('fastavro.cfastavro', [pyx])
     ext_modules = [fastavro]
     cmdclass = {'build_ext': build_ext}
 except ImportError:
@@ -18,6 +23,8 @@ setup(
     author_email='miki.tebeka@gmail.com',
     license='MIT',
     url='https://bitbucket.org/tebeka/fastavro',
+    packages=['fastavro'],
     ext_modules=ext_modules,
     cmdclass=cmdclass,
+    zip_safe=False,
 )
