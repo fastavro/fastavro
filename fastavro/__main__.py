a0e5e4439090194da680da8c094f80099a0b1c04
Miki Tebeka <miki.tebeka@gmail.com>
2012-01-25 15:36:58 -0800

    Several files and encoding

--- fastavro/__main__.py
+++ fastavro/__main__.py
@@ -1,5 +1,8 @@
 import fastavro as avro
 import json
+from sys import stdout
+
+encoding = stdout.encoding or "UTF-8"
 
 def main(argv=None):
     import sys
@@ -9,32 +12,33 @@ def main(argv=None):
 
     parser = ArgumentParser(version=avro.__version__,
         description='iter over avro file, emit records as JSON')
-    parser.add_argument('filename', help='file to parse', nargs='?')
-    parser.add_argument('--schema', help='dump schema and exit',
+    parser.add_argument('file', help='file(s) to parse', nargs='+')
+    parser.add_argument('--schema', help='dump schema instead of records',
                         action='store_true', default=False)
     args = parser.parse_args(argv[1:])
 
-    if args.filename:
-        try:
-            fo = open(args.filename, 'rb')
-        except IOError as e:
-            raise SystemExit(
-                'error: cannot open {0} - {1}'.format(args.filename, e))
-    else:
-        fo = sys.stdin
+    for filename in args.file:
+        if filename == '-':
+            fo = sys.stdin
+        else:
+            try:
+                fo = open(filename, 'rb')
+            except IOError as e:
+                raise SystemExit(
+                    'error: cannot open {0} - {1}'.format(args.filename, e))
 
-    stdout = sys.stdout
-    reader = avro.reader(fo)
-    if args.schema:
-        json.dump(reader.schema, stdout, indent=4)
-        raise SystemExit
-
-    try:
-        for record in reader:
-            json.dump(record, stdout)
+        reader = avro.reader(fo)
+        if args.schema:
+            json.dump(reader.schema, stdout, indent=4, encoding=encoding)
             sys.stdout.write('\n')
-    except (IOError, KeyboardInterrupt):
-        pass
+            continue
+
+        try:
+            for record in reader:
+                json.dump(record, stdout, encoding=encoding)
+                sys.stdout.write('\n')
+        except (IOError, KeyboardInterrupt):
+            pass
 
 if __name__ == '__main__':
     main()
