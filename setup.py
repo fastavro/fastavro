a0d3d5a16ac15f252580eceffc526f843bd6ae5a
Miki Tebeka <miki.tebeka@gmail.com>
2012-01-25 13:42:38 -0800

    Optionally build extension (stolen from simplejson)

--- setup.py
+++ setup.py
@@ -2,9 +2,31 @@ import fastavro
 
 from setuptools import setup
 from distutils.core import Extension
+import distutils.log as log
 from os.path import join
+from distutils.command.build_ext import build_ext
+from distutils.errors import CCompilerError, DistutilsExecError, \
+    DistutilsPlatformError
 
 cfile = join('fastavro', 'cfastavro.c')
+
+ext_errors = (CCompilerError, DistutilsExecError, DistutilsPlatformError,
+              IOError)
+
+class maybe_build_ext(build_ext):
+    '''This class allows C extension building to fail.'''
+
+    def run(self):
+        try:
+            build_ext.run(self)
+        except DistutilsPlatformError:
+            log.info('cannot bulid C extension, will continue without.')
+
+    def build_extension(self, ext):
+        try:
+            build_ext.build_extension(self, ext)
+        except ext_errors:
+            log.info('cannot bulid C extension, will continue without.')
 
 setup(
     name='fastavro',
@@ -16,6 +38,7 @@ setup(
     license='MIT',
     url='https://bitbucket.org/tebeka/fastavro',
     packages=['fastavro'],
-    ext_modules=[Extension('fastavro.cfastavro', [cfile], optional=True)],
+    ext_modules=[Extension('fastavro.cfastavro', [cfile])],
+    cmdclass={'build_ext' : maybe_build_ext},
     zip_safe=False,
 )
