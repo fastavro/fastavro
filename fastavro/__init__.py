8a71bb3c3d3e2ba2e4499e7861c210d61cff1e40
Miki Tebeka <miki.tebeka@gmail.com>
2012-02-22 17:40:57 -0800

    0.5.0

--- fastavro/__init__.py
+++ fastavro/__init__.py
@@ -16,7 +16,7 @@ The only onterface function is iter_avro, example usage::
 '''
 
 __all__ = [ 'iter_avro' ]
-__version__ = '0.4.2'
+__version__ = '0.5.0'
 
 try:
     from . import cfastavro as _avro
