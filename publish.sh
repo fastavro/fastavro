72359bc0f17d34e65e7c472cb3b01fde3961ea9b
Miki Tebeka <miki.tebeka@gmail.com>
2012-03-10 13:33:14 -0800

    0.6.0

--- publish.sh
+++ publish.sh
@@ -2,6 +2,7 @@
 # Push to pypi, tag and push to bitbucket
 
 python setup.py bdist_egg upload
+python3 setup.py bdist_egg upload
 python setup.py sdist upload
 rm -fr build dist
 hg tag -f $(python setup.py --version)
