0d8c743c34d55c3fb38af9660a02a261be3c4297
Miki Tebeka <miki.tebeka@gmail.com>
2012-03-12 10:34:23 -0700

    Specify which nosetets to run and pass arguments
    --HG--
    branch : py3k-2

--- run-tests.sh
+++ run-tests.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+# Run tests suite, you can specify which nose to run by setting environment
+# variable `nose` (running without will use `nosetests`).
+#   nose=nosetests-3.2 ./run-tests.sh
 
-nose=${1-nosetests}
-
-$nose -vd tests
+nose=${nose-nosetests}
+$nose -vd $@ tests
