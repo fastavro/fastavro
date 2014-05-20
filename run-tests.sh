d8033b20377c123b9598ec02e1fb05122026baa7
Miki Tebeka <miki.tebeka@gmail.com>
2014-05-20 06:45:57 +0300

    more flake8 cleanup and test_requires

--- run-tests.sh
+++ run-tests.sh
@@ -6,8 +6,13 @@
 echo "[$(date)] Running tests on $(hostname) [user=${USER}, pwd=${PWD}]"
 echo
 
+echo "running flake8"
+flake8 fastavro tests
+if [ $? -ne 0 ]; then
+    exit $?
+fi
+
 nose=${nose-nosetests}
 echo "nose is $nose"
-
 
 $nose -vd $@ tests
