816ae48aba9658e089a4b33ab9138e9ddf4817fa
Miki Tebeka <miki.tebeka@gmail.com>
2015-08-05 20:09:41 +0300

    ooops, forgot to add

---
+++ install-py35.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+curl -LO https://www.python.org/ftp/python/3.5.0/Python-3.5.0b2.tar.xz
+tar -xJf Python-3.5.0b2.tar.xz
+cd Python-3.5.0b2
+./configure --prefix=/opt/py35 && make install
