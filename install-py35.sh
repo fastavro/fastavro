93252fdd1e39807a66e33318839da3115aabd93d
Miki Tebeka <miki.tebeka@gmail.com>
2015-08-05 20:24:30 +0300

    Install python 3.5 to somewhere writable

--- install-py35.sh
+++ install-py35.sh
@@ -3,4 +3,4 @@
 curl -LO https://www.python.org/ftp/python/3.5.0/Python-3.5.0b2.tar.xz
 tar -xJf Python-3.5.0b2.tar.xz
 cd Python-3.5.0b2
-./configure --prefix=/opt/py35 && make install
+./configure --prefix=${HOME}/.local && make install
