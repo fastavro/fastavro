7f05b83380f84f18f613dc48b1a75abc3697519e
Miki Tebeka <miki.tebeka@gmail.com>
2015-08-13 11:02:22 +0300

    Fixed bug in version

--- install-py35.sh
+++ install-py35.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-curl -LO https://www.python.org/ftp/python/3.5.0/Python-3.5.0rc1.tar.xz
-tar -xJf Python-3.5.0b2.tar.xz
-cd Python-3.5.0b2
+version=Python-3.5.0rc1
+archive=${version}.tar.xz
+
+curl -LO https://www.python.org/ftp/python/3.5.0/${archive}
+tar -xJf ${archive}
+cd ${version}
 ./configure --prefix=${HOME}/.local && make install
