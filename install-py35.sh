8a2fc2c4ecab90000b281c3889ff3be9dbb667b5
Miki Tebeka <miki.tebeka@gmail.com>
2015-08-13 10:47:48 +0300

    Use 3.5rc1 for testing on travis

--- install-py35.sh
+++ install-py35.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -LO https://www.python.org/ftp/python/3.5.0/Python-3.5.0b2.tar.xz
+curl -LO https://www.python.org/ftp/python/3.5.0/Python-3.5.0rc1.tar.xz
 tar -xJf Python-3.5.0b2.tar.xz
 cd Python-3.5.0b2
 ./configure --prefix=${HOME}/.local && make install
