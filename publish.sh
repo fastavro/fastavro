ec5f8a202bcc45b90282b9dc052acb709f1c2812
Miki Tebeka <miki.tebeka@gmail.com>
2015-08-21 07:49:33 +0300

    fix error in conda upload

--- publish.sh
+++ publish.sh
@@ -3,12 +3,13 @@
 
 # Fail on 1'st error
 set -e
+set -x
 
 make
 python3 setup.py bdist_egg upload
 python2 setup.py bdist_egg upload
 python setup.py sdist upload
-/opt/anaconda/bin/conda bdist_conda --binstar-upload
+/opt/anaconda/bin/python setup.py bdist_conda --binstar-upload
 rm -fr build dist fastavro.egg-info/
 git tag -f $(python setup.py --version)
 git push --tags
