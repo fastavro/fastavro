73a655a088603eb33f9d0ead8c7b94bb9221fbb6
Miki Tebeka <miki.tebeka@gmail.com>
2015-12-27 14:02:30 +0200

    fix publish

--- publish.sh
+++ publish.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # Push to pypi, tag and push to bitbucket
 
+pyver=$(python -c 'import sys; print("%s%s" % sys.version_info[:2])')
+ver=$(python setup.py --version)
+pkg=fastavro-${ver}-py${pyver}_0.tar.bz
+
+
 # Fail on 1'st error
 set -e
 set -x
@@ -9,7 +14,8 @@ make
 python3 setup.py bdist_egg upload
 python2 setup.py bdist_egg upload
 python setup.py sdist upload
-/opt/anaconda/bin/python setup.py bdist_conda --binstar-upload
+/opt/anaconda/bin/python setup.py bdist_conda
+/opt/anaconda/bin/anaconda upload /opt/anaconda/conda-bld/linux-64/${pkg}
 rm -fr build dist fastavro.egg-info/
 git tag -f $(python setup.py --version)
 git push
