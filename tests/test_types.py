e964e44d2fe6a0181290ab04c8b274f62b0bdcff
Miki Tebeka <miki.tebeka@gmail.com>
2016-06-12 12:48:26 +0300

    Type mistmatch tests (from issue #37)

---
+++ tests/test_types.py
@@ -0,0 +1,91 @@
+from io import BytesIO
+import fastavro
+
+from nose.tools import raises
+
+schema = {
+  "fields": [
+    {
+      "name": "str_null",
+      "type": ["null", "string"]
+    },
+    {
+      "name": "str",
+      "type": "string"
+    },
+    {
+      "name": "integ_null",
+      "type": ["null", "int"]
+    },
+    {
+      "name": "integ",
+      "type": "int"
+    }
+  ],
+  "namespace": "namespace",
+  "name": "missingerror",
+  "type": "record"
+}
+
+
+def serialize(schema, *records):
+    buffer = BytesIO()
+    fastavro.writer(buffer, schema, records)
+    serialized = buffer.getvalue()
+    return serialized
+
+
+def test_types_match():
+    records = [{
+        'str_null': 'str',
+        'str': 'str',
+        'integ_null': 21,
+        'integ': 21,
+    }]
+    serialize(schema, *records)
+
+
+@raises(TypeError, ValueError)
+def test_string_in_int_raises():
+    records = [{
+        'str_null': 'str',
+        'str': 'str',
+        'integ_null': 'str',
+        'integ': 21,
+    }]
+
+    serialize(schema, *records)
+
+
+@raises(TypeError, ValueError)
+def test_string_in_int_null_raises():
+    records = [{
+        'str_null': 'str',
+        'str': 'str',
+        'integ_null': 11,
+        'integ': 'str',
+    }]
+    serialize(schema, *records)
+
+
+@raises(TypeError, ValueError)
+def test_int_in_string_null_raises():
+    records = [{
+        'str_null': 11,
+        'str': 'str',
+        'integ_null': 21,
+        'integ': 21,
+    }]
+    serialize(schema, *records)
+
+
+@raises(TypeError, ValueError, AttributeError)
+def test_int_in_string_raises():
+    records = [{
+        'str_null': 'str',
+        'str': 11,
+        'integ_null': 21,
+        'integ': 21,
+    }]
+
+    serialize(schema, *records)
