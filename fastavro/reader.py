54e7c19a381b4e77dfb0f76a87816a9834a7211b
Kurt Ostfeld <kurtostfeld@gmail.com>
2016-07-22 16:19:46 -0500

    Made fix to reader.py for reader schemas with default values of None. Added corresponding unit tests

--- fastavro/reader.py
+++ fastavro/reader.py
@@ -341,7 +341,7 @@ def read_record(fo, writer_schema, reader_schema=None):
             for field_name, field in iteritems(readers_field_dict):
                 if field_name not in writer_fields:
                     default = field.get('default')
-                    if default:
+                    if 'default' in field:
                         record[field['name']] = default
                     else:
                         msg = 'No default value for %s' % field['name']
