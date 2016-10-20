78cbbd901b4a65e25230a894bfa81d06cae3eb1a
Miki Tebeka <miki.tebeka@gmail.com>
2016-10-20 07:10:10 +0300

    0.11.0

--- fastavro/__init__.py
+++ fastavro/__init__.py
@@ -42,8 +42,8 @@ The only onterface function is iter_avro, example usage::
         writer(out, schema, records)
 '''
 
-__version__ = '0.10.2'
-__version_info__ = (0, 10, 2)
+__version__ = '0.11.0'
+__version_info__ = (0, 11, 0)
 
 
 try:
