dfb4cc73a9f0f86f75ab69756caece7733b3fbae
Barry Hart <barrywhart@yahoo.com>
2017-11-21 19:51:37 -0500

    Disable some bogus flake8 warnings

--- fastavro/schema.py
+++ fastavro/schema.py
@@ -1,7 +1,6 @@
-from _schema_public import UnknownType
+from _schema_public import UnknownType  # noqa: F401
 
 try:
-    from ._schema import acquaint_schema, load_schema
+    from ._schema import acquaint_schema, load_schema  # noqa: F401
 except ImportError:
-    from ._schema_py import acquaint_schema, load_schema
-
+    from ._schema_py import acquaint_schema, load_schema  # noqa: F401
