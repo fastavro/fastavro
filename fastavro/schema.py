8211bcfbeb5566be6892811a358148c238d75d81
Barry Hart <barrywhart@yahoo.com>
2017-11-21 20:00:00 -0500

    Use relative import to make Python 3 work correctly

--- fastavro/schema.py
+++ fastavro/schema.py
@@ -1,4 +1,4 @@
-from _schema_public import UnknownType  # noqa: F401
+from ._schema_public import UnknownType  # noqa: F401
 
 try:
     from ._schema import acquaint_schema, load_schema  # noqa: F401
