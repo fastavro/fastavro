479239d34a715d781179674f42a5e2115dd0af24
scottbelden <scottabelden@gmail.com>
2018-02-02 08:50:38 -0500

    fix path in container

--- .travis_build_wheel.sh
+++ .travis_build_wheel.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 cd `dirname $0`
-TRAVIS_PYTHON_VERSION=$1
 
-"/opt/python/$TRAVIS_PYTHON_VERSION/pip" install cython
-"/opt/python/$TRAVIS_PYTHON_VERSION/python" setup.py bdist_wheel
+# The version comes in as something like 3.6, but we need it to be 36 for the
+# path in the container so we remove the period
+TRAVIS_PYTHON_VERSION=$1
+TRAVIS_PYTHON_VERSION=${TRAVIS_PYTHON_VERSION/.}
+
+"/opt/python/*${TRAVIS_PYTHON_VERSION}*m/pip" install cython
+"/opt/python/*${TRAVIS_PYTHON_VERSION}*m/python" setup.py bdist_wheel
