71d0a4a35daba564cb9f13c2771147ce4754952d
scottbelden <scottabelden@gmail.com>
2018-02-13 18:55:48 -0500

    fix the docs path

--- docs/conf.py
+++ docs/conf.py
@@ -12,12 +12,12 @@
 # serve to show the default.
 
 import sys, os
-import fastavro
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
+import fastavro
 
 # -- General configuration -----------------------------------------------------
 
