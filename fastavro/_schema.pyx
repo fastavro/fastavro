5f7e5fe3c69161fb0cf7eaedc9bb99286e21758f
scottbelden <scottabelden@gmail.com>
2018-10-30 18:55:03 -0400

    set cython level to 3

--- fastavro/_schema.pyx
+++ fastavro/_schema.pyx
@@ -1,3 +1,4 @@
+# cython: language_level=3str
 # cython: auto_cpdef=True
 
 from os import path
--- fastavro/_six.pyx
+++ fastavro/_six.pyx
@@ -1,3 +1,4 @@
+# cython: language_level=3str
 # cython: auto_cpdef=True
 
 '''Compatiblity for Python versions.
