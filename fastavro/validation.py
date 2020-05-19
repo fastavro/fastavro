ad666b34e94b868f942472dc35eae31b1db6463b
Spencer Nelson <swnelson@uw.edu>
2020-05-19 16:39:49 -0700

    Add type stubs for validation APIs

--- fastavro/validation.py
+++ fastavro/validation.py
@@ -1,11 +1,11 @@
 try:
     from . import _validation
 except ImportError:
-    from . import _validation_py as _validation
+    from . import _validation_py as _validation # type: ignore
 from ._validate_common import ValidationErrorData, ValidationError
 
 # Private API
-_validate = _validation._validate
+_validate = _validation._validate # type: ignore
 
 # Public API
 validate = _validation.validate
