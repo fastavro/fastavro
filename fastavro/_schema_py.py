cedaa25a76676090e7794ba89841a4eb0ff9533e
Jakob van Santen <jvansanten@gmail.com>
2023-12-20 09:23:01 +0100

    Allow arbitrary ordering of union fields
    Since the Java reference implementation no longer enforces the
    requirement that the default value be the first type in a union,
    drop this requirement from fastavro as well.  This restores the
    ability to read data written with fastavro <1.7, while still checking
    that the default is (or is convertible to) one of the union types.
    Along the way, refactor the special cases for double and float so
    that they do not raise exceptions.
    
    Fixes #723

--- fastavro/_schema_py.py
+++ fastavro/_schema_py.py
@@ -322,16 +322,39 @@ def parse_schema(
 
 
 def _raise_default_value_error(
-    default: Any, schema_type: str, in_union: bool, ignore_default_error: bool
+    default: Any, schema_type: Any, in_union: bool, ignore_default_error: bool
 ):
     if ignore_default_error:
         return
     elif in_union:
-        text = f"first schema in union with type: {schema_type}"
+        text = f"a schema in union with type: {schema_type}"
     else:
         text = f"schema type: {schema_type}"
 
     raise SchemaParseException(f"Default value <{default}> must match {text}")
+
+
+def _maybe_float(value: Any) -> Any:
+    try:
+        return float(value)
+    except (TypeError, ValueError):
+        return value
+
+
+def _default_matches_schema(default: Any, schema: Schema) -> bool:
+    # TODO: Consider using the validate functions here
+    if (
+        (schema == "null" and default is not None)
+        or (schema == "boolean" and not isinstance(default, bool))
+        or (schema == "string" and not isinstance(default, str))
+        or (schema == "bytes" and not isinstance(default, str))
+        or (schema == "double" and not isinstance(_maybe_float(default), float))
+        or (schema == "float" and not isinstance(_maybe_float(default), float))
+        or (schema == "int" and not isinstance(default, int))
+        or (schema == "long" and not isinstance(default, int))
+    ):
+        return False
+    return True
 
 
 def _parse_schema(
@@ -348,52 +371,32 @@ def _parse_schema(
 ) -> Schema:
     # union schemas
     if isinstance(schema, list):
-        parsed_schemas = []
-        for index, s in enumerate(schema):
-            if index == 0:
-                parsed_schemas.append(
-                    _parse_schema(
-                        s,
-                        namespace,
-                        expand,
-                        False,
-                        names,
-                        named_schemas,
-                        default,
-                        ignore_default_error,
-                        in_union=True,
-                    )
-                )
+        parsed_schemas = [
+            _parse_schema(
+                s,
+                namespace,
+                expand,
+                False,
+                names,
+                named_schemas,
+                NO_DEFAULT,
+                ignore_default_error,
+            )
+            for s in schema
+        ]
+        if default is not NO_DEFAULT:
+            for s in parsed_schemas:
+                if _default_matches_schema(default, s):
+                    break
             else:
-                parsed_schemas.append(
-                    _parse_schema(
-                        s,
-                        namespace,
-                        expand,
-                        False,
-                        names,
-                        named_schemas,
-                        NO_DEFAULT,
-                        ignore_default_error,
-                    )
-                )
+                _raise_default_value_error(default, schema, True, ignore_default_error)
         return parsed_schemas
 
     # string schemas; this could be either a named schema or a primitive type
     elif not isinstance(schema, dict):
         if schema in PRIMITIVES:
             if default is not NO_DEFAULT:
-                # TODO: Consider using the validate functions here
-                if (
-                    (schema == "null" and default is not None)
-                    or (schema == "boolean" and not isinstance(default, bool))
-                    or (schema == "string" and not isinstance(default, str))
-                    or (schema == "bytes" and not isinstance(default, str))
-                    or (schema == "double" and not isinstance(float(default), float))
-                    or (schema == "float" and not isinstance(float(default), float))
-                    or (schema == "int" and not isinstance(default, int))
-                    or (schema == "long" and not isinstance(default, int))
-                ):
+                if not _default_matches_schema(default, schema):
                     _raise_default_value_error(
                         default, schema, in_union, ignore_default_error
                     )
