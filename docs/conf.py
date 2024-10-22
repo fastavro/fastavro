925b652e37a61b9e820f2d34468695190518825d
scottbelden <scottabelden@gmail.com>
2024-10-22 18:40:17 -0400

    fix get_html_theme_path deprecation

--- docs/conf.py
+++ docs/conf.py
@@ -108,7 +108,6 @@ on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 if not on_rtd:
     import sphinx_rtd_theme
     html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
